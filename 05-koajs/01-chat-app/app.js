0ae2c08a3c7c4b93fa2f215472ae44c458e7a678
nikita <nishkrut@yandex.ru>
2022-03-23 23:46:54 +0300

    task 05-01

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -1,17 +1,34 @@
 const path = require('path');
 const Koa = require('koa');
+const static = require('koa-static');
 const app = new Koa();
 
-app.use(require('koa-static')(path.join(__dirname, 'public')));
+app.use(static(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
+
 const router = new Router();
+const resolvers = [];
 
 router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await new Promise((res) => {
+    resolvers.push(res);
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (!message) {
+    ctx.body = '';
+    return;
+  }
+
+  for (const res of resolvers) {
+    res(message);
+  }
+
+  ctx.body = '';
 });
 
 app.use(router.routes());
