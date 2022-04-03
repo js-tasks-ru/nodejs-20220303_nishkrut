1abd983e40b8d3b73cbb1100ad36df67c7a2a53c
nikita <nishkrut@yandex.ru>
2022-04-03 22:19:04 +0300

    fix task 07-02

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -5,43 +5,23 @@ module.exports = async function authenticate(strategy, email, displayName, done)
     return done(null, false, 'Не указан email');
   }
 
-  const emailIsValid = validateEmail(email);
+  try {
+    const user = await User.findOne({email: email});
 
-  if (!emailIsValid) {
-    // console.log(email);
-    const errors = {
-      email: {
-        message: 'Некорректный email.',
-      },
-    };
+    if (!user) {
+      const user = {
+        'email': email,
+        'displayName': displayName,
+        'social': strategy,
+      };
 
-    const emailError = {
-      name: 'ValidationError',
-      errors: errors,
-    };
-
-    return done(emailError, false);
-  }
-
-  const user = await User.findOne({email: email});
-
-  if (!user) {
-    const user = {
-      'email': email,
-      'displayName': displayName,
-      'social': strategy,
-    };
-
-    const u = new User(user);
-    await u.save();
+      const u = new User(user);
+      await u.save();
+      return done(null, user);
+    }
     return done(null, user);
+  } catch (err) {
+    done(err, false);
   }
-  return done(null, user);
-
   // done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
 };
-
-function validateEmail(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
